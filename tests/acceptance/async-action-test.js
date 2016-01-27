440d538901093652a90ccbe137fedea60c3e594f
Dave Wasmer <davewasmer@gmail.com>
2016-01-27 15:00:02 -0700

    wip

---
+++ tests/acceptance/async-action-test.js
@@ -0,0 +1,32 @@
+/* jshint expr:true */
+import {
+  describe,
+  it,
+  beforeEach,
+  afterEach
+} from 'mocha';
+import { expect } from 'chai';
+import startApp from '../helpers/start-app';
+import destroyApp from '../helpers/destroy-app';
+
+describe('Acceptance: AsyncAction', function() {
+  let application;
+
+  beforeEach(function() {
+    application = startApp();
+  });
+
+  afterEach(function() {
+    destroyApp(application);
+  });
+
+  it('can visit /async-action', function() {
+    visit('/');
+
+    click('button');
+
+    andThen(function() {
+      console.log('App has settled');
+    });
+  });
+});
